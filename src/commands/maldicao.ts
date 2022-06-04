b140fd20e25f40f206c52720b176a2b9453cd6f9
leozinho <leonardo_gurgel2@hotmail.com>
2022-06-03 23:19:06 -0300

    adicionando socos, rasteiras, maldicoes e todas aquelas coisas que a gente ama

--- src/commands/maldicao.ts
+++ src/commands/maldicao.ts
@@ -2,8 +2,20 @@ import { send } from '../utils';
 import type { Command } from '.';
 
 const praga = [
-  "ser afogadu num tanque cheio de tubarões por um psicopata gostoso yandere que canta bem que não aceita o fim da relação",
-  "virar um proletariadu que trabalha numa mina de amianto pra ganhar um pacote de doritos por dia pra alimentar a esposa e 300 filhos enquanto nada numa represa de lama tóxica da mineradora over rica que não liga pra ele."
+  "ser afogade num tanque cheio de tubarões por um psicopata gostoso yandere que canta bem que não aceita o fim da relação",
+  "virar um proletariade que trabalha numa mina de amianto pra ganhar um pacote de doritos por dia pra alimentar a esposa e 300 filhos enquanto nada numa represa de lama tóxica da mineradora over rica que não liga pra elu.",
+  "tomar um rojão nuclear no meio do C# e sair voando até a orbita de netuno batendo em cada lixo espacial que a humanidade fidida tacou no ceu pra atropelar os pombos",
+  "passar os próximos 300 séculos programando em java",
+  "ser elu mesme.",
+  "a se casar comigo",
+  "passar a vida inteira procurando o amor da vida delu sem se tocar que nem a mãe delu u ama",
+  "cair na malha fina da receita federal",
+  "tirar foto pro instagram sem maquiagem nem photoshop",
+  "parar de se iludir e acordar para a verdade sobre a vida e o universo e nunca mais se iludir",
+  "nunca mais na vida usar calmantes não regulamentados pela anvisa",
+  "cursar a faculdade de novo",
+  "virar saco de pancada de canguru boxeador viciado em anabolizantes esteroidados",
+  "convencer a aws a cobrar menos de 10 USD / mes pra rodar as coisas delu"
 ]
 
 const get_offense = () => praga[Math.floor(Math.random()*praga.length)]
@@ -12,7 +24,7 @@ export const maldicao: Command = {
   aliases: ['maldicao', 'praga', 'coisaruim', 'ritual'],
   exec: async (input, args, user) => {
     send(
-      `${args.join(" ")} vai ${get_offense()} PunOko`
+      `${args.join(" ")} foi amaldiçoadu a ${get_offense()} PunOko`
     );
   },
 };
--- src/commands/punch.ts
+++ src/commands/punch.ts
@@ -5,7 +5,8 @@ const praga = [
   "vai a nocaute beijando o chão com muito amor",
   "sai voando, entra em orbita, bate com a cara na estação espacial e abre um buraco novo na lua",
   "voa de cara na menina feia e psicopata, da um beijo nela e é forçado a se casar com ela e viver sofrendo para sempre",
-  "perde um dente da boca que cai no esgoto, vira geosmina, para na baia de guanabara, é comido por um peixe mutante que é vendido na feira que é encontrado pelo rei salomão que oferece como tributo para a fada do dente e usa a grana para recomprar o reino perdido de nosgorth"
+  "perde um dente da boca que cai no esgoto, vira geosmina, para na baia de guanabara, é comido por um peixe mutante que é vendido na feira que é encontrado pelo rei salomão que oferece como tributo para a fada do dente e usa a grana para recomprar o reino perdido de nosgorth",
+  "vai a nocaute e perde o UFC - ultimate fapping contest"
 ]
 
 const get_offense = () => praga[Math.floor(Math.random()*praga.length)]
