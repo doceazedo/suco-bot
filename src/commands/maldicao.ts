6b968a627941d607c09cdf8647fdf3b71972dca3
leozinho <leonardo_gurgel2@hotmail.com>
2022-06-03 22:06:01 -0300

    corrigindo erro de portugues

--- src/commands/maldicao.ts
+++ src/commands/maldicao.ts
@@ -3,7 +3,7 @@ import type { Command } from '.';
 
 const praga = [
   "ser afogadu num tanque cheio de tubarões por um psicopata gostoso yandere que canta bem que não aceita o fim da relação",
-  "virar um proletariadu ue trabalha numa mina de amianto pra ganhar um pacote de doritos por dia pra alimentar a esposa e 300 filhos enquanto nada numa represa de lama tóxica da mineradora over rica que não liga pra ele."
+  "virar um proletariadu que trabalha numa mina de amianto pra ganhar um pacote de doritos por dia pra alimentar a esposa e 300 filhos enquanto nada numa represa de lama tóxica da mineradora over rica que não liga pra ele."
 ]
 
 const get_offense = () => praga[Math.floor(Math.random()*praga.length)]
