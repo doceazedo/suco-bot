82a0928e41566a16d0954ed95f2aa96b432b1e7c
leozinho <38364010+leoggo@users.noreply.github.com>
2022-06-08 20:53:30 -0300

    Update src/commands/tronco.ts
    Co-authored-by: DoceAzedo <lucasfernandesforever@gmail.com>

--- src/commands/tronco.ts
+++ src/commands/tronco.ts
@@ -8,34 +8,13 @@ const randomNumber = () => Math.floor(Math.random() * (30 - 1) + 1)
 const makeTronco = () => {
     const size = randomNumber()
 
-    if (size < 5) {
-        return [size, 'pipico piquitito BibleThump']
-    }
-
-    if (size < 10) {
-        return [size, 'faz um esforço que fica aceitavel VoHiYo']
-    }
-
-    if (size < 13) {
-        return [size, 'médio KonCha']
-    }
-
-    if (size < 16) {
-        return [size, 'uie... TehePelo']
-    }
-
-    if (size < 20) {
-        return [size, 'maior que o meu PunOko']
-    }
-
-    if (size < 25) {
-        return [size, 'bem maior que o meu NotLikeThis']
-    }
-
-    if (size < 28) {
-        return [size, 'aberração genetica imensa... PogChamp']
-    }
-
+    if (size < 5) return [size, 'pipico piquitito BibleThump'];
+    if (size < 10) return [size, 'faz um esforço que fica aceitavel VoHiYo'];
+    if (size < 13) return [size, 'médio KonCha'];
+    if (size < 16) return [size, 'uie... TehePelo'];
+    if (size < 20) return [size, 'maior que o meu PunOko'];
+    if (size < 25) return [size, 'bem maior que o meu NotLikeThis'];
+    if (size < 28) return [size, 'aberração genetica imensa... PogChamp'];
     return [size, 'parabéns... você passou da minha regua... ta feliz? eu não to não... some daqui, vai']
 }
 
