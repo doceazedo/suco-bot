cf779d9eabf348dd178676e1e76bedbe69681b72
leozinho <38364010+leoggo@users.noreply.github.com>
2022-06-08 20:56:53 -0300

    Update src/commands/rasteira.ts
    Co-authored-by: DoceAzedo <lucasfernandesforever@gmail.com>

--- src/commands/rasteira.ts
+++ src/commands/rasteira.ts
@@ -1,7 +1,7 @@
 import { send } from '../utils';
 import type { Command } from '.';
 
-const mensagem = [
+const offenses = [
   'quebra a cara no chão, e quebra o chão junto, cai na banheira da vizinha, morre afogade, e toma uma sentença post mortem de indenizar a vizinha por danos banheirais',
   'rola a escada de 1000 andares do templo shaolin, cai pela janela da cobertura de um rpédio de 300 andares, é atropeladu por um drone, bate de cara numa tampa de boeiro explosiva, se afoga num rio de escremento e vira geosmina pro pessoal beber',
   'toma um tombo de classe social, vira um crackudo mizeravel e fica vagando pela estação da luz dando dinheiro pra traficante que da dinheiro pra puliça corrupta que da dinheiro pra político corrupto que da compra de votos pra proletariado gado retardado que compra iphones fididos que só duram 2 dias e vão parar em lixão infectando o solo com chumbo so pra dar dinheiro pro grande capital',
