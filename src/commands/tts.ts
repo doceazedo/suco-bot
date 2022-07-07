866318044fe38fad3195855442b68aad764b2da8
DoceAzedo <me@doceazedo.com>
2022-07-06 23:13:43 -0300

    Add !tts skip

--- src/commands/tts.ts
+++ src/commands/tts.ts
@@ -1,4 +1,4 @@
-import { replyError } from '../utils';
+import { broadcast, isModerator, replyError } from '../utils';
 import { pollyVoices } from '../helpers';
 import type { Command } from '.';
 import { playGoogleTTS, playPollyTTS } from '../utils/play-tts';
@@ -11,6 +11,12 @@ export const tts: Command = {
         user,
         'veja como usar e quais vozes estão disponíveis em https://doceazedo.com/tts'
       );
+
+    if (args[0] == 'skip') {
+      if (!isModerator(user))
+        return replyError(user, 'Somente mods podem usar esse comando 🤭');
+      return broadcast('skipaudio');
+    }
 
     const voiceId =
       args[0].charAt(0).toUpperCase() + args[0].slice(1).toLowerCase();
